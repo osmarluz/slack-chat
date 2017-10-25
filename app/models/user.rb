1efa283b35fcd9f8749676382d6575139a120513
osmarluz <osmar.luz@gmail.com>
2017-10-25 21:16:16 -0200

    Update of user and team models relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,13 @@ class User < ApplicationRecord
   has_many :teams
   has_many :messages
   has_many :talks, dependent: :destroy
-  
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :team_users, dependent: :destroy
+  has_many :team_memberships, through: :team_users, :source => :team
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  def my_teams
+    self.teams + self.team_memberships
+  end
 end
